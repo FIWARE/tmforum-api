91feac7f8be1fab7e0d2a30abca44095ce346c41
Stefan Wiedemann <wistefan@googlemail.com>
2023-02-27 11:04:11 +0100

    trigger ct

--- product-inventory/src/test/java/org/fiware/tmforum/productinventory/ProductApiIT.java
+++ product-inventory/src/test/java/org/fiware/tmforum/productinventory/ProductApiIT.java
@@ -42,6 +42,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+
 import java.net.URI;
 import java.time.Instant;
 import java.util.ArrayList;
