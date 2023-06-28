312a7413abbb75bb9f6fd864114d7c237a2bb1a4
Stefan Wiedemann <wistefan@googlemail.com>
2023-06-28 10:50:41 +0200

    clean up

--- party-catalog/src/main/java/org/fiware/tmforum/party/rest/IndividualApiController.java
+++ party-catalog/src/main/java/org/fiware/tmforum/party/rest/IndividualApiController.java
@@ -1,12 +1,8 @@
 package org.fiware.tmforum.party.rest;
 
 import io.micronaut.core.annotation.NonNull;
-import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
-import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.QueryValue;
-import io.micronaut.http.context.ServerRequestContext;
 import lombok.extern.slf4j.Slf4j;
 import org.fiware.party.api.IndividualApi;
 import org.fiware.party.model.IndividualCreateVO;
@@ -15,7 +11,6 @@ import org.fiware.party.model.IndividualVO;
 import org.fiware.tmforum.common.exception.TmForumException;
 import org.fiware.tmforum.common.exception.TmForumExceptionReason;
 import org.fiware.tmforum.common.mapping.IdHelper;
-import org.fiware.tmforum.common.querying.QueryParser;
 import org.fiware.tmforum.common.repository.TmForumRepository;
 import org.fiware.tmforum.common.validation.ReferenceValidationService;
 import org.fiware.tmforum.common.validation.ReferencedEntity;
@@ -29,10 +24,8 @@ import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Controller("${general.basepath:/}")
