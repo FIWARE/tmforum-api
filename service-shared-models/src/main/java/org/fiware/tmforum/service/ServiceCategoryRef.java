6db93e7e0e6a9d9b7bfa8022d333bfc7174be7a5
mvazquezficodes <mvazquez@ficodes.com>
2023-11-22 15:31:42 +0100

    Reverting changes to ServiceCategoryRef and commenting resource catalog pom for testing purposes

--- service-shared-models/src/main/java/org/fiware/tmforum/service/ServiceCategoryRef.java
+++ service-shared-models/src/main/java/org/fiware/tmforum/service/ServiceCategoryRef.java
@@ -1,18 +1,17 @@
 package org.fiware.tmforum.service;
 
+import io.github.wistefan.mapping.annotations.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import org.fiware.tmforum.common.domain.RefEntity;
-import io.github.wistefan.mapping.annotations.AttributeGetter;
-import io.github.wistefan.mapping.annotations.AttributeSetter;
-import io.github.wistefan.mapping.annotations.AttributeType;
-import io.github.wistefan.mapping.annotations.Ignore;
+import org.fiware.tmforum.servicecatalog.domain.ServiceSpecification;
 
 import java.net.URI;
 import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
+@MappingEnabled(entityType = ServiceCategory.TYPE_SERVICE_CATEGORY)
 public class ServiceCategoryRef extends RefEntity {
 
     @Getter(onMethod = @__({@AttributeGetter(value = AttributeType.PROPERTY, targetName = "version", embedProperty = true)}))
