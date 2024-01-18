6e642ee3f9d8d829d317ee2caba646f5f23a8321
beknazaresenbek <129863747+beknazaresenbek@users.noreply.github.com>
2024-01-18 11:25:19 +0100

    Updated api table links (#45)
    * Updated api table links
    
    * Fixed tests in resource function activation

--- resource-function-activation/src/main/java/org/fiware/tmforum/resourcefunction/domain/Connection.java
+++ resource-function-activation/src/main/java/org/fiware/tmforum/resourcefunction/domain/Connection.java
@@ -3,15 +3,15 @@ package org.fiware.tmforum.resourcefunction.domain;
 import lombok.Data;
 
 import java.net.URI;
-import java.util.List;
 
 @Data
 public class Connection {
 
     private String id;
+    private URI href;
     private String associationType;
     private String name;
-    private List<EndpointRef> endpoint;
+    private EndpointRef endpoint;
     private String atBaseType;
     private URI atSchemaLocation;
     private String atType;
