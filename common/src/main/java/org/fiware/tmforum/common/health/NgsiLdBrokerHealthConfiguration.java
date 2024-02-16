d37ce724a0b4d160c7a9463e802f44b973416fb2
Tim Smyth <33017641+pulledtim@users.noreply.github.com>
2024-02-16 16:21:14 +0100

    readiness check (#47)
    * readiness check
    
    * liveness check

---
+++ common/src/main/java/org/fiware/tmforum/common/health/NgsiLdBrokerHealthConfiguration.java
@@ -0,0 +1,14 @@
+package org.fiware.tmforum.common.health;
+
+import io.micronaut.context.annotation.ConfigurationProperties;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.core.util.StringUtils;
+import io.micronaut.management.endpoint.health.HealthEndpoint;
+
+import java.util.List;
+
+@ConfigurationProperties(HealthEndpoint.PREFIX + ".broker")
+@Requires(property = HealthEndpoint.PREFIX + ".broker.enabled", defaultValue = StringUtils.FALSE, notEquals = StringUtils.FALSE)
+public record NgsiLdBrokerHealthConfiguration (boolean enabled, List<Integer> allowedResponseCodes){
+
+}
