9e560cd513ec1ced07d9a250e51e73523bf8bb5a
Stefan Wiedemann <wistefan@googlemail.com>
2025-01-30 12:48:45 +0100

    fix ref

--- product-shared-models/src/main/java/org/fiware/tmforum/product/ProductOfferingPriceRefValue.java
+++ product-shared-models/src/main/java/org/fiware/tmforum/product/ProductOfferingPriceRefValue.java
@@ -3,11 +3,24 @@ package org.fiware.tmforum.product;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.github.wistefan.mapping.annotations.MappingEnabled;
+import lombok.EqualsAndHashCode;
+import org.fiware.tmforum.common.domain.RefEntity;
 import org.fiware.tmforum.common.domain.ReferenceValue;
 
-public class ProductOfferingPriceRefValue extends ReferenceValue{
-    @Override
-    public List<String> getReferencedTypes() {
-        return new ArrayList<>(List.of("product-offering-price"));
-    }
+@MappingEnabled(entityType = {"product-offering-price"})
+@EqualsAndHashCode(callSuper = true)
+public class ProductOfferingPriceRefValue extends RefEntity {
+
+	protected ProductOfferingPriceRefValue(@JsonProperty("id") String id) {
+		super(id);
+	}
+
+	@Override
+	@JsonIgnore
+	public List<String> getReferencedTypes() {
+		return new ArrayList<>(List.of("product-offering-price"));
+	}
 }
