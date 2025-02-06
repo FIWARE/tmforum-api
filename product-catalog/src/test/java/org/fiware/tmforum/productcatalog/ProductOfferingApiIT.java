1308b9daefc78631cd8179566d5387434735f61c
Stefan Wiedemann <wistefan@googlemail.com>
2025-02-06 15:14:45 +0100

    enable again

--- product-catalog/src/test/java/org/fiware/tmforum/productcatalog/ProductOfferingApiIT.java
+++ product-catalog/src/test/java/org/fiware/tmforum/productcatalog/ProductOfferingApiIT.java
@@ -816,7 +816,6 @@ public class ProductOfferingApiIT extends AbstractApiIT implements ProductOfferi
 				"The correct productOffering should be returned.");
 	}
 
-	@Disabled("Needs to be temporarily disabled, since orion-ld fails the test due to https://github.com/FIWARE/context.Orion-LD/issues/1741")
 	@MethodSource("extendedOfferingProvider")
 	@ParameterizedTest
 	public void retrieveProductOfferingWithExtension200(ProductOfferingCreateVO productOfferingCreateVO, ProductOfferingVO expectedProductOffering, String message) throws Exception {
