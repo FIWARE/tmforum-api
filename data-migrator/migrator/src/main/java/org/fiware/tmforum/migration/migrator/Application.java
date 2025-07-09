990ca554d09f0b8f17d4367574159ce8f0f489c5
Stefan Wiedemann <wistefan@googlemail.com>
2025-07-09 12:30:35 +0200

    add commandline options

--- data-migrator/migrator/src/main/java/org/fiware/tmforum/migration/migrator/Application.java
+++ data-migrator/migrator/src/main/java/org/fiware/tmforum/migration/migrator/Application.java
@@ -2,31 +2,37 @@ package org.fiware.tmforum.migration.migrator;
 
 import java.net.MalformedURLException;
 
-//@CommandLine.Command(name = "data-migrator", description = "Migration tool for transfering data between TMForum 0.32.x and 1.3.x", mixinStandardHelpOptions = true)
-public class Application {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
 
-	//	@CommandLine.Option(names = {"-rB", "--readBroker"}, description = "Address of the broker to read from.")
-//	String readBroker;
-//
-//	@CommandLine.Option(names = {"-wB", "--writeBroker"}, description = "Address of the broker to write from.")
-//	String writeBroker;
-//
-//	@CommandLine.Option(names = {"-ll", "--legacyLoader"}, description = "Path to the jar-file of the legacy loader.")
-	private static String legacyLoaderJar = "data-migrator/legacy-loader/target/legacy-loader-0.1.jar";
-	//
-//	@CommandLine.Option(names = {"-uw", "--updateWriter"}, description = "Path to the jar-file of the update writer.")
-	private static String updateWriterJar = "data-migrator/update-writer/target/update-writer-0.1.jar";
+@Command(name = "data-migrator", description = "Migration tool for transfering data between TMForum 0.32.x and 1.3.x", mixinStandardHelpOptions = true)
+public class Application implements Runnable {
+
+	@CommandLine.Option(names = {"-rB", "--readBroker"}, description = "Address of the broker to read from.", defaultValue = "http://localhost:8080")
+	String readBroker;
+
+	@CommandLine.Option(names = {"-wB", "--writeBroker"}, description = "Address of the broker to write from.", defaultValue = "http://localhost:1026")
+	String writeBroker;
+
+	@CommandLine.Option(names = {"-ll", "--legacyLoader"}, description = "Path to the jar-file of the legacy loader.", defaultValue = "data-migrator/legacy-loader/target/legacy-loader-0.1.jar")
+	String legacyLoaderJar;
+
+	@CommandLine.Option(names = {"-uw", "--updateWriter"}, description = "Path to the jar-file of the update writer.", defaultValue = "data-migrator/update-writer/target/update-writer-0.1.jar")
+	String updateWriterJar;
 
 	public static void main(String[] args) {
+		CommandLine.run(new Application(), args);
 
-		Migrator migrator = new Migrator("http://localhost:8080", "http://localhost:1026", legacyLoaderJar, updateWriterJar);
+	}
+
+	@Override
+	public void run() {
+		Migrator migrator = new Migrator(readBroker, writeBroker, legacyLoaderJar, updateWriterJar);
 		try {
 			migrator.runUpdate();
 		} catch (MalformedURLException e) {
 			throw new RuntimeException(e);
 		}
 		System.exit(0);
-
 	}
-
 }
