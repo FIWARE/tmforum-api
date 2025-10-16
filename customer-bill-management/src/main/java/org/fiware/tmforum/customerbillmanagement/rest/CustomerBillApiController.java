b7d54ead263057eb6a5f140da63db27a217dbc7e
Daniel Ros <d.ros@libelium.com>
2025-10-16 11:49:17 +0200

    added remove request for bill management

--- customer-bill-management/src/main/java/org/fiware/tmforum/customerbillmanagement/rest/CustomerBillApiController.java
+++ customer-bill-management/src/main/java/org/fiware/tmforum/customerbillmanagement/rest/CustomerBillApiController.java
@@ -104,4 +104,9 @@ public class CustomerBillApiController extends AbstractApiController<CustomerBil
 				.map(tmForumMapper::map)
 				.map(HttpResponse::ok);
 	}
+
+	@Override
+	public Mono<HttpResponse<Object>> deleteCustomerBill(String id) {
+		return delete(id);
+	}
 }
