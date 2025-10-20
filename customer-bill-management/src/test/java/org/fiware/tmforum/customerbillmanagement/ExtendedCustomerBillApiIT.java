a3c1c7c434a434b020d69cb73aacd76727ac7278
Daniel Ros <d.ros@libelium.com>
2025-10-20 08:53:11 +0200

    implemented delete method on extended class

--- customer-bill-management/src/test/java/org/fiware/tmforum/customerbillmanagement/ExtendedCustomerBillApiIT.java
+++ customer-bill-management/src/test/java/org/fiware/tmforum/customerbillmanagement/ExtendedCustomerBillApiIT.java
@@ -6,12 +6,13 @@ import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.test.annotation.MockBean;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
-import org.fiware.customerbillmanagement.api.ext.AppliedCustomerBillingRateApiTestSpec;
+import org.fiware.customerbillmanagement.api.CustomerBillApiTestClient;
 import org.fiware.customerbillmanagement.api.ext.CustomerBillExtensionApiTestClient;
 import org.fiware.customerbillmanagement.api.ext.CustomerBillExtensionApiTestSpec;
 import org.fiware.customerbillmanagement.model.*;
 import org.fiware.ngsi.api.EntitiesApiClient;
 import org.fiware.tmforum.common.configuration.GeneralProperties;
+import org.fiware.tmforum.common.exception.ErrorDetails;
 import org.fiware.tmforum.common.test.AbstractApiIT;
 import org.fiware.tmforum.customerbillmanagement.domain.CustomerBill;
 import org.junit.jupiter.api.Disabled;
@@ -24,12 +25,11 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.net.URI;
 import java.time.Clock;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -38,6 +38,7 @@ import static org.mockito.Mockito.when;
 public class ExtendedCustomerBillApiIT extends AbstractApiIT implements CustomerBillExtensionApiTestSpec {
 
 	private final CustomerBillExtensionApiTestClient testClient;
+	private final CustomerBillApiTestClient customerBillApiTestClient;
 
 	private String message;
 	private CustomerBillCreateVO customerBillCreateVO;
@@ -50,9 +51,10 @@ public class ExtendedCustomerBillApiIT extends AbstractApiIT implements Customer
 	}
 
 
-	protected ExtendedCustomerBillApiIT(EntitiesApiClient entitiesApiClient, ObjectMapper objectMapper, GeneralProperties generalProperties, CustomerBillExtensionApiTestClient testClient) {
+	protected ExtendedCustomerBillApiIT(EntitiesApiClient entitiesApiClient, ObjectMapper objectMapper, GeneralProperties generalProperties, CustomerBillExtensionApiTestClient testClient, CustomerBillApiTestClient customerBillApiTestClient) {
 		super(entitiesApiClient, objectMapper, generalProperties);
 		this.testClient = testClient;
+		this.customerBillApiTestClient = customerBillApiTestClient;
 	}
 
 	@Test
@@ -159,6 +161,94 @@ public class ExtendedCustomerBillApiIT extends AbstractApiIT implements Customer
 
 	@Override
 	public void createCustomerBill500() throws Exception {
+
+	}
+
+	@Test
+	@Override
+	public void deleteCustomerBill204() throws Exception {
+
+		//first create
+		CustomerBillCreateVO createVO = CustomerBillCreateVOTestExample.build()
+				.atSchemaLocation(null)
+				.billingAccount(null)
+				.financialAccount(null)
+				.paymentMethod(null);
+
+		HttpResponse<CustomerBillVO> customerBillVOHttpResponse = callAndCatch(
+				() -> testClient.createCustomerBill(null,
+						createVO));
+		assertEquals(HttpStatus.CREATED, customerBillVOHttpResponse.getStatus(), "The bill should have successfully been created.");
+		String billId = customerBillVOHttpResponse.body().getId();
+
+		assertEquals(HttpStatus.NO_CONTENT,
+				callAndCatch(() -> testClient.deleteCustomerBill(null, billId)).getStatus(),
+				"The customer bill should have been deleted.");
+
+		assertEquals(HttpStatus.NOT_FOUND,
+				callAndCatch(() -> customerBillApiTestClient.retrieveCustomerBill(null, billId, null)).status(),
+				"The customer bill should not exist anymore.");
+
+	}
+
+	@Disabled("400 is impossible to happen on deletion with the current implementation.")
+	@Test
+	@Override
+	public void deleteCustomerBill400() throws Exception {
+
+	}
+
+	@Disabled("Security is handled externally, thus 401 and 403 cannot happen.")
+	@Test
+	@Override
+	public void deleteCustomerBill401() throws Exception {
+
+	}
+
+	@Disabled("Security is handled externally, thus 401 and 403 cannot happen.")
+	@Test
+	@Override
+	public void deleteCustomerBill403() throws Exception {
+
+	}
+
+	@Test
+	@Override
+	public void deleteCustomerBill404() throws Exception {
+		HttpResponse<?> notFoundResponse = callAndCatch(
+				() -> testClient.deleteCustomerBill(null, "urn:ngsi-ld:customer-bill:no-bill"));
+		assertEquals(HttpStatus.NOT_FOUND,
+				notFoundResponse.getStatus(),
+				"No such bill should exist.");
+
+		Optional<ErrorDetails> optionalErrorDetails = notFoundResponse.getBody(ErrorDetails.class);
+		assertTrue(optionalErrorDetails.isPresent(), "Error details should be provided.");
+
+		notFoundResponse = callAndCatch(() -> testClient.deleteCustomerBill(null, "invalid-id"));
+		assertEquals(HttpStatus.NOT_FOUND,
+				notFoundResponse.getStatus(),
+				"No such bill should exist.");
+
+		optionalErrorDetails = notFoundResponse.getBody(ErrorDetails.class);
+		assertTrue(optionalErrorDetails.isPresent(), "Error details should be provided.");
+	}
+
+	@Disabled("Prohibited by the framework.")
+	@Test
+	@Override
+	public void deleteCustomerBill405() throws Exception {
+
+	}
+
+	@Disabled("Impossible status.")
+	@Test
+	@Override
+	public void deleteCustomerBill409() throws Exception {
+
+	}
+
+	@Override
+	public void deleteCustomerBill500() throws Exception {
 
 	}
 
