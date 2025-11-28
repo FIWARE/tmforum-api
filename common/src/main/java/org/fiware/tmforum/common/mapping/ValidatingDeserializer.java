3f10d93cfa7d155cc7d79679b522f9ca8cc87098
Daniel Ros <d.ros@libelium.com>
2025-11-28 09:37:20 +0100

    avoid base field substitution

--- common/src/main/java/org/fiware/tmforum/common/mapping/ValidatingDeserializer.java
+++ common/src/main/java/org/fiware/tmforum/common/mapping/ValidatingDeserializer.java
@@ -3,6 +3,7 @@ package org.fiware.tmforum.common.mapping;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.deser.std.DelegatingDeserializer;
+import com.fasterxml.jackson.databind.introspect.BeanPropertyDefinition;
 import com.fasterxml.jackson.databind.util.TokenBuffer;
 import com.networknt.schema.*;
 import com.networknt.schema.resource.ClasspathSchemaLoader;
@@ -16,9 +17,12 @@ import org.fiware.tmforum.common.exception.SchemaValidationException;
 import javax.validation.ValidationException;
 import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Deserializer, that validates incoming objects against the linked(atSchemaLocation) json-schema.
@@ -78,6 +82,7 @@ public class ValidatingDeserializer extends DelegatingDeserializer {
 			SchemaValidatorsConfig.Builder validatorConfigBuilder = SchemaValidatorsConfig.builder();
 			SchemaValidatorsConfig schemaValidatorsConfig = validatorConfigBuilder.build();
 			JsonSchema schema = jsonSchemaFactory.getSchema(SchemaLocation.of(schemaAddress), schemaValidatorsConfig);
+			checkForExplicitFieldOverrides(schema);
 			Set<ValidationMessage> assertions = schema.validate(jsonString, InputFormat.JSON, executionContext -> {
 				executionContext.getExecutionConfig().setFormatAssertionsEnabled(true);
 			});
@@ -94,4 +99,78 @@ public class ValidatingDeserializer extends DelegatingDeserializer {
 		}
 
 	}
+
+	private void checkForExplicitFieldOverrides(JsonSchema schema) {
+		var schemaNode = schema.getSchemaNode();
+		var propertiesNode = schemaNode.get("definitions");
+
+		if (propertiesNode == null || !propertiesNode.isObject()) {
+			log.debug("Schema has no properties node, skipping override check");
+			return;
+		}
+
+		// Get all properties from BeanDescription (includes Jackson-visible properties)
+		Set<String> existingProperties = new HashSet<>();
+		for (BeanPropertyDefinition prop : beanDescription.findProperties()) {
+			existingProperties.add(prop.getName());
+		}
+
+		// Also get all field names from the class hierarchy
+		existingProperties.addAll(getAllFieldNames(beanDescription.getBeanClass()));
+
+		// Add standard TMForum properties that should never be overridden by schemas
+		existingProperties.add("id");
+		existingProperties.add("href");
+		existingProperties.add("@baseType");
+		existingProperties.add("@type");
+		existingProperties.add("@schemaLocation");
+
+		log.debug("Final existing properties found in {}: {}", beanDescription.getBeanClass().getSimpleName(), existingProperties);
+
+		// Check for overlaps and collect all violations
+		List<String> overriddenFields = new ArrayList<>();
+		propertiesNode.fieldNames().forEachRemaining(schemaField -> {
+			if (existingProperties.contains(schemaField)) {
+				overriddenFields.add(schemaField);
+			}
+		});
+
+		// If any fields are being overridden, throw an error
+		if (!overriddenFields.isEmpty()) {
+			String entityName = beanDescription.getBeanClass().getSimpleName();
+			String errorMessage = String.format(
+				"Schema validation failed for entity '%s': The schema attempts to override the following base properties: %s. " +
+				"These properties are part of the entity definition and cannot be redefined in the schema.",
+				entityName,
+				String.join(", ", overriddenFields)
+			);
+			log.error(errorMessage);
+			throw new SchemaValidationException(List.of(errorMessage), errorMessage);
+		}
+	}
+
+	private Set<String> getAllFieldNames(Class<?> clazz) {
+		Set<String> fieldNames = new HashSet<>();
+		Class<?> current = clazz;
+
+		log.debug("Getting all fields for class: {}", clazz.getName());
+
+		// Traverse the class hierarchy
+		while (current != null && current != Object.class) {
+			log.debug("  Checking class: {}", current.getSimpleName());
+			java.lang.reflect.Field[] fields = current.getDeclaredFields();
+			log.debug("    Found {} fields", fields.length);
+			for (java.lang.reflect.Field field : fields) {
+				log.debug("      Field: {} (type: {})", field.getName(), field.getType().getSimpleName());
+				fieldNames.add(field.getName());
+			}
+			current = current.getSuperclass();
+			if (current != null && current != Object.class) {
+				log.debug("    Moving to superclass: {}", current.getSimpleName());
+			}
+		}
+
+		return fieldNames;
+	}
+
 }
