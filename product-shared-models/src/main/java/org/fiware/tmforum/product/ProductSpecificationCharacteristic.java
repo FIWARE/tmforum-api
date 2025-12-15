ed7fa2db209149b341cec5d21c33c82207fa288d
Daniel Ros <d.ros@libelium.com>
2025-12-15 09:08:38 +0100

    extend

--- product-shared-models/src/main/java/org/fiware/tmforum/product/ProductSpecificationCharacteristic.java
+++ product-shared-models/src/main/java/org/fiware/tmforum/product/ProductSpecificationCharacteristic.java
@@ -1,11 +1,16 @@
 package org.fiware.tmforum.product;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import org.fiware.tmforum.common.domain.TimePeriod;
 import org.fiware.tmforum.service.CharacteristicValueSpecification;
 
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Data
 public class ProductSpecificationCharacteristic {
@@ -27,4 +32,28 @@ public class ProductSpecificationCharacteristic {
 	private String atBaseType;
 	private URI atSchemaLocation;
 	private String atType;
+
+	@JsonIgnore
+	private Map<String, Object> additionalProperties = new HashMap<>();
+
+	@JsonAnyGetter
+	public Map<String, Object> getAdditionalProperties() {
+		return this.additionalProperties;
+	}
+
+	@JsonAnySetter
+	public void setAdditionalProperty(String propertyKey, Object value) {
+		if (this.additionalProperties == null) {
+			this.additionalProperties = new HashMap<>();
+		}
+		this.additionalProperties.put(propertyKey, value);
+	}
+
+	@JsonIgnore
+	public void addAdditionalProperty(String propertyKey, Object value) {
+		if (this.additionalProperties == null) {
+			this.additionalProperties = new HashMap<>();
+		}
+		this.additionalProperties.put(propertyKey, value);
+	}
 }
--- service-shared-models/src/main/java/org/fiware/tmforum/service/CharacteristicValueSpecification.java
+++ service-shared-models/src/main/java/org/fiware/tmforum/service/CharacteristicValueSpecification.java
@@ -1,9 +1,14 @@
 package org.fiware.tmforum.service;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import org.fiware.tmforum.common.domain.TimePeriod;
 
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 @Data
 public class CharacteristicValueSpecification {
@@ -20,4 +25,28 @@ public class CharacteristicValueSpecification {
     private String atBaseType;
     private URI atSchemaLocation;
     private String atType;
+
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<>();
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String propertyKey, Object value) {
+        if (this.additionalProperties == null) {
+            this.additionalProperties = new HashMap<>();
+        }
+        this.additionalProperties.put(propertyKey, value);
+    }
+
+    @JsonIgnore
+    public void addAdditionalProperty(String propertyKey, Object value) {
+        if (this.additionalProperties == null) {
+            this.additionalProperties = new HashMap<>();
+        }
+        this.additionalProperties.put(propertyKey, value);
+    }
 }
