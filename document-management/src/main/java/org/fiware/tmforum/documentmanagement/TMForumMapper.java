8d8e431dad343b93e1bcb074c820701218fbf268
Daniel Ros <d.ros@libelium.com>
2026-03-12 12:35:52 +0100

    Added document API

---
+++ document-management/src/main/java/org/fiware/tmforum/documentmanagement/TMForumMapper.java
@@ -0,0 +1,131 @@
+package org.fiware.tmforum.documentmanagement;
+
+import io.github.wistefan.mapping.MappingException;
+import org.fiware.document.model.DocumentSpecificationCreateVO;
+import org.fiware.document.model.DocumentSpecificationUpdateVO;
+import org.fiware.document.model.DocumentSpecificationVO;
+import org.fiware.document.model.AttachmentRefOrValueVO;
+import org.fiware.tmforum.common.domain.AttachmentRefOrValue;
+import org.fiware.tmforum.common.domain.subscription.TMForumSubscription;
+import org.fiware.tmforum.common.mapping.BaseMapper;
+import org.fiware.tmforum.common.mapping.IdHelper;
+import org.fiware.tmforum.documentmanagement.domain.DocumentSpecification;
+import org.mapstruct.AfterMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+@Mapper(componentModel = "jsr330", uses = {IdHelper.class})
+public abstract class TMForumMapper extends BaseMapper {
+
+    // DocumentSpecification mappings
+
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "href", source = "id")
+    @Mapping(target = "atSchemaLocation", ignore = true)
+    public abstract DocumentSpecificationVO map(DocumentSpecificationCreateVO createVO, URI id);
+
+    @AfterMapping
+    protected void afterMapCreateVO(DocumentSpecificationCreateVO source, @MappingTarget DocumentSpecificationVO target) {
+        if (source != null) {
+            target.setAtSchemaLocation(source.getAtSchemaLocation());
+        }
+    }
+
+    @Mapping(target = "atSchemaLocation", ignore = true)
+    public abstract DocumentSpecificationVO map(DocumentSpecification documentSpecification);
+
+    @AfterMapping
+    protected void afterMapEntity(DocumentSpecification source, @MappingTarget DocumentSpecificationVO target) {
+        if (source != null) {
+            target.setAtSchemaLocation(source.getAtSchemaLocation());
+        }
+    }
+
+    @Mapping(target = "href", source = "id")
+    @Mapping(target = "atSchemaLocation", ignore = true)
+    public abstract DocumentSpecification map(DocumentSpecificationVO documentSpecificationVO);
+
+    @AfterMapping
+    protected void afterMapVO(DocumentSpecificationVO source, @MappingTarget DocumentSpecification target) {
+        if (source != null) {
+            target.setAtSchemaLocation(source.getAtSchemaLocation());
+        }
+    }
+
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "href", source = "id")
+    @Mapping(target = "atSchemaLocation", ignore = true)
+    public abstract DocumentSpecificationVO map(DocumentSpecificationUpdateVO updateVO, String id);
+
+    @AfterMapping
+    protected void afterMapUpdateVO(DocumentSpecificationUpdateVO source, @MappingTarget DocumentSpecificationVO target) {
+        if (source != null) {
+            target.setAtSchemaLocation(source.getAtSchemaLocation());
+        }
+    }
+
+    // AttachmentRefOrValue mappings
+
+    @Mapping(target = "atSchemaLocation", ignore = true)
+    public abstract AttachmentRefOrValue map(AttachmentRefOrValueVO attachmentVO);
+
+    @AfterMapping
+    protected void afterMapAttachmentVO(AttachmentRefOrValueVO source, @MappingTarget AttachmentRefOrValue target) {
+        if (source != null) {
+            target.setAtSchemaLocation(source.getAtSchemaLocation());
+        }
+    }
+
+    @Mapping(target = "atSchemaLocation", ignore = true)
+    public abstract AttachmentRefOrValueVO map(AttachmentRefOrValue attachment);
+
+    @AfterMapping
+    protected void afterMapAttachment(AttachmentRefOrValue source, @MappingTarget AttachmentRefOrValueVO target) {
+        if (source != null) {
+            target.setAtSchemaLocation(source.getAtSchemaLocation());
+        }
+    }
+
+    // URL/URI helper methods
+
+    public URL map(String value) {
+        if (value == null) {
+            return null;
+        }
+        try {
+            return new URL(value);
+        } catch (MalformedURLException e) {
+            throw new MappingException(String.format("%s is not a URL.", value), e);
+        }
+    }
+
+    public String map(URL value) {
+        if (value == null) {
+            return null;
+        }
+        return value.toString();
+    }
+
+    public URI map(URI value) {
+        return value;
+    }
+
+    public URI mapToURI(String value) {
+        if (value == null) {
+            return null;
+        }
+        return URI.create(value);
+    }
+
+    public String mapFromURI(URI value) {
+        if (value == null) {
+            return null;
+        }
+        return value.toString();
+    }
+}
