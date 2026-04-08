ff33c34f26fed50cb2e2f0c3149a4db2c10b5d2f
Víctor Rampérez Martín <vramperez@MacBook-Air-de-Victor.local>
2026-04-08 12:52:29 +0200

    fix(subtypes): handle @type in ValidatingDeserializer and add missing @MappingEnabled subtypes to Ref classes

--- resource-shared-models/src/main/java/org/fiware/tmforum/resource/ResourceRef.java
+++ resource-shared-models/src/main/java/org/fiware/tmforum/resource/ResourceRef.java
@@ -4,10 +4,21 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.EqualsAndHashCode;
 import org.fiware.tmforum.common.domain.RefEntity;
+import io.github.wistefan.mapping.annotations.MappingEnabled;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@MappingEnabled(entityType = {
+        Resource.TYPE_RESOURCE,
+        PhysicalResource.TYPE_PHYSICAL_RESOURCE,
+        LogicalResource.TYPE_LOGICAL_RESOURCE,
+        SoftwareResource.TYPE_SOFTWARE_RESOURCE,
+        InstalledSoftware.TYPE_INSTALLED_SOFTWARE,
+        SoftwareSupportPackage.TYPE_SOFTWARE_SUPPORT_PACKAGE,
+        ApiResource.TYPE_API_RESOURCE,
+        HostingPlatformRequirement.TYPE_HOSTING_PLATFORM_REQUIREMENT
+})
 @EqualsAndHashCode(callSuper = true)
 public class ResourceRef extends RefEntity {
 
