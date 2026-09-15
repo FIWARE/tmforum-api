7ac2222da8f08e149cf55a8c9ed85aedd10ba08b
Miguel Ortega <miguel.ortega@seamware.com>
2026-09-15 13:12:35 +0200

    update AbstractApiControllerTest to new micronaut 4 api

--- common/src/test/java/org/fiware/tmforum/common/rest/AbstractApiControllerTest.java
+++ common/src/test/java/org/fiware/tmforum/common/rest/AbstractApiControllerTest.java
@@ -9,7 +9,6 @@ import org.fiware.tmforum.common.querying.MyPojo;
 import org.fiware.tmforum.common.querying.QueryParser;
 import org.fiware.tmforum.common.repository.PagedResult;
 import org.fiware.tmforum.common.repository.TmForumRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -35,11 +34,6 @@ class AbstractApiControllerTest {
 		}
 	}
 
-	@AfterEach
-	public void clearRequestContext() {
-		ServerRequestContext.set(null);
-	}
-
 	@Test
 	public void listPolymorphicForwardsTheIdFilterToTheRepository() {
 		// Regression test: listPolymorphic used to only forward "type" and "query" from the
@@ -52,14 +46,15 @@ class AbstractApiControllerTest {
 		TestController controller = new TestController(queryParser, repository);
 
 		String requestedId = "urn:ngsi-ld:software-specification:0e2d5c4a-cf51-43cf-a510-dff06f62f4a3";
-		ServerRequestContext.set(
-				HttpRequest.GET("/resourceSpecification?id=" + requestedId + "&fields=lifecycleStatus"));
+		HttpRequest<?> request = HttpRequest.GET("/resourceSpecification?id=" + requestedId + "&fields=lifecycleStatus");
 
 		when(repository.findEntitiesPolymorphic(any(), any(), any(), any(), any(), any(), any()))
 				.thenReturn(Mono.just(new PagedResult<>(List.of(), 0, 10, null)));
 
-		controller.listPolymorphic(0, 10, "software-specification,resource-specification", MyPojo.class,
-				type -> MyPojo.class).block();
+		ServerRequestContext.with(request, () -> {
+			controller.listPolymorphic(0, 10, "software-specification,resource-specification", MyPojo.class,
+					type -> MyPojo.class).block();
+		});
 
 		verify(repository).findEntitiesPolymorphic(any(), any(), any(), any(), eq(requestedId), any(), any());
 	}
